783b441438eb5ebdbee9b9f22009cac531b90223
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-31 14:34:49 +0530

    Added new user registration functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,39 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @route   POST /api/users
+// @desc    Register new user
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User data')
+  }
+})
+
 // @route   GET /api/users/profile
 // @desc    Get user profile
 // @access  Private
@@ -43,4 +76,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
